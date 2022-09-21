f1438692d3440ba5e7ed6e94edb9a7d6ed2502d8
Amir Hamza <ah.zahed00@gmail.com>
2022-09-21 09:56:57 +0600

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,7 +42,7 @@ export default {
     ],
   },
   publicRuntimeConfig: {
-    baseURL: process.env.BASE_URL || 'http://uapapi.uap-bd.edu/',
+    baseURL: process.env.BASE_URL || 'https://uapadmin.shoptheowlet.com/',
   },
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['@/styles/app.scss'],
@@ -72,7 +72,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
-    baseURL: 'http://uapapi.uap-bd.edu/api',
+    baseURL: 'https://uap.shoptheowlet.com/api/',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
